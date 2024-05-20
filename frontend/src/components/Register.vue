828248d92feccaf7222e64e8dcb2aadb00934ae9
yourboyfred <fredrickkimathi26@gmail.com>
2024-05-20 14:12:12 +0300

    database connection

---
+++ frontend/src/components/Register.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="register-page">
+    <div class="google">
+      <div class="google-button" @click="socialLogin">
+        <img class="google-icon" href="#" src="@/assets/googleicon.png"    alt="Image alt"/>
+        <a class="google-word">Continue with Google</a>
+      </div>
+    </div>
+    <p>OR</p>
+    <form @submit.prevent="register" class="register-child">
+      <input type="email" placeholder="Email address..." v-model="email" />
+      <input type="password" placeholder="password..." v-model="password" />
+      <button type="submit">Register</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    socialLogin() {
+      // Handle social login
+    },
+    register() {
+      axios.post('/api/register', {
+        email: this.email,
+        password: this.password,
+      })
+      .then(response => {
+        // Handle successful registration
+      })
+      .catch(error => {
+        // Handle error
+      });
+    },
+  },
+};
+</script>
